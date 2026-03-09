b649d4aa2b0134a4cc84e38d3eb3fb4f193fb7cb
ibo-beep <hojeijibrahim14@gmail.com>
2026-03-09 23:01:04 +0100

    Mobile Funktion optimiert

--- app/pages/laylatulqadr.vue
+++ app/pages/laylatulqadr.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="flex flex-row flex-nowrap gap-1 p-1 w-full h-screen overflow-hidden">
+  <div class="flex flex-row text-center overflow-x-auto gap-4 p-4 md:flex-wrap md:justify-center">
     
     <div 
       v-for="(category, catIndex) in duaCategories" 
       :key="catIndex"
-      class="bg-white p-1 rounded-lg shadow-sm border border-gray-200 w-1/3 flex-shrink-0 flex flex-col"
+      class="bg-white p-4 rounded-3xl shadow-sm border border-gray-200 min-w-[280px] md:w-[30%]"
     >
-      <h2 class="text-[20px] md:text-xl font-bold text-gray-800 mb-1 border-b pb-1 border-green-300 text-center truncate">
+      <h2 class="text-lg font-bold text-gray-800 mb-4 border-b pb-4 border-green-300">
         {{ category.name }}
       </h2>
 
-      <div class="space-y-0.5 overflow-y-auto">
+      <div class="space-y-2">
         <NuxtLink 
           v-for="(item, itemIndex) in category.links" 
           :key="itemIndex"
           :to="item.url"
-          class="block p-1.5 bg-gray-50 hover:bg-gray-200 transition-colors rounded border-l-2 border-green-600 text-gray-700 text-[16px] md:text-sm font-bold truncate"
+          class="block p-3 bg-gray-50 hover:bg-gray-200 transition-colors rounded-lg border-l-4 border-green-600 text-gray-700 text-sm font-bold truncate"
         >
           {{ item.title }}
         </NuxtLink>
@@ -31,12 +31,12 @@ const duaCategories = [
     name: "Nacht 1",
     links: [
       { title: "Sure al-Qadr", url: "/qadr" },
-      { title: "Dua Abu Hamza", url: "/abu_hamza" },  
-      { title: "Dua Jawshan", url: "/jawshan" },
+      { title: "Dua Abu Hamza al-Thumali", url: "/abu_hamza" },  
+      { title: "Dua Jawshan al Kabeer", url: "/jawshan" },
       { title: "Dua Kumail", url: "/kumail" },
-      { title: "Dua amsaytu", url: "/allahumma" },
-      { title: "Koran-Bittgebet", url: "/bittgebet_koran" },
-      { title: "Ziyarat Hussain", url: "/ziyarat_hussain" },
+      { title: "Dua Allahumma inni amsaytu", url: "/allahumma" },
+      { title: "Bittgebet des Korans", url: "/bittgebet_koran" },
+      { title: "Ziyarat Imam al Hussain", url: "/ziyarat_hussain" },
       { title: "Ziyarat Ashura", url: "/ashura" },
     ]
   },
@@ -44,13 +44,13 @@ const duaCategories = [
     name: "Nacht 2",
     links: [
       { title: "Sure al-Qadr", url: "/qadr" },
-      { title: "Dua Abu Hamza", url: "/abu_hamza" },  
-      { title: "Dua Jawshan", url: "/jawshan" },
+      { title: "Dua Abu Hamza al-Thumali", url: "/abu_hamza" },  
+      { title: "Dua Jawshan al Kabeer", url: "/jawshan" },
       { title: "Dua Kumail", url: "/kumail" },
-      { title: "Dua amsaytu", url: "/allahumma" },
-      { title: "Koran-Bittgebet", url: "/bittgebet_koran" },
-      { title: "Ziyarat Hussain", url: "/ziyarat_hussain" },
-      { title: "Ziyarat Ali", url: "/ziyarat_ali" },
+      { title: "Dua Allahumma inni amsaytu", url: "/allahumma" },
+      { title: "Bittgebet des Korans", url: "/bittgebet_koran" },
+      { title: "Ziyarat Imam al Hussain a.s.", url: "/ziyarat_hussain" },
+      { title: "Ziyarat Imam Ali a.s.", url: "/ziyarat_ali" },
       { title: "Ziyarat Ashura", url: "/ashura" },
       { title: "Ziyarat Ameenallah", url: "/ziyarat_ameenallah" },
     ]
@@ -63,16 +63,16 @@ const duaCategories = [
       { title: "Sure ad-Dukhan", url: "/ad_dukhan" },
       { title: "Sure al-Qadr", url: "/qadr" },
       { title: "Dua al-Iftitah", url: "/iftitah" },
-      { title: "Dua Sajjad", url: "/dua_sajjad" },
-      { title: "Dua Abu Hamza", url: "/abu_hamza" },  
-      { title: "Dua Jawshan", url: "/jawshan" },
+      { title: "Dua Imam Sajjad a.s.", url: "/dua_sajjad" },
+      { title: "Dua Abu Hamza al-Thumali", url: "/abu_hamza" },  
+      { title: "Dua Jawshan al Kabeer", url: "/jawshan" },
       { title: "Dua Kumail", url: "/kumail" },
-      { title: "Dua amsaytu", url: "/allahumma" },
-      { title: "Koran-Bittgebet", url: "/bittgebet_koran" },
-      { title: "Ziyarat Ali Akbar", url: "/ali_al_akbar" },
-      { title: "Ziyarat Shohadaa", url: "/shohadaa" },
-      { title: "Ziyarat Abbas", url: "/abal_fadl" },
-      { title: "Ziyarat Hussain", url: "/ziyarat_hussain" },
+      { title: "Dua Allahumma inni amsaytu", url: "/allahumma" },
+      { title: "Bittgebet des Korans", url: "/bittgebet_koran" },
+      { title: "Ziyarat Ali Al Akbar", url: "/ali_al_akbar" },
+      { title: "Ziyarat Shohadaa Karbala", url: "/shohadaa" },
+      { title: "Ziyarat Abu Fadl Abbas", url: "/abal_fadl" },
+      { title: "Ziyarat Imam al Hussain", url: "/ziyarat_hussain" },
       { title: "Ziyarat Ashura", url: "/ashura" },
     ]
   }
